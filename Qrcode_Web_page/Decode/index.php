cf5e7cbef8b48aa977be80fd500bd1865d75e76c
Ayabiten <ach.yabiii@gmail.com>
2019-02-01 15:56:16 +0100

    Qrcode_web_page
    a simple page can decode qrcode or generate it

---
+++ Qrcode_Web_page/Decode/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Decoding QR codes</title>
+     <meta name="description" content="QR Code scanner" />
+  <meta name="keywords" content="scanner,barcode" />
+  <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  
+  <link rel="stylesheet" href="css/bootstrap.min.css" >
+<link rel="stylesheet" href="css/bootstrap.css" > 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    <style>
+        html, body {
+            height: 100%;
+            width: 100%;
+        }
+        .bg {
+            background-image: url("images/bg.jpg");
+            height: 100%;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+    </style>
+</head>
+<body >
+    <header>
+<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+    <img src="img/logo.jpg" alt="" width="150" height="40">
+  </a>
+</nav>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+ 
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="./../index.html">Scan <span class="sr-only">(current)</span></a>
+      </li>
+	  <li class="nav-item active">
+        <a class="nav-link" href="/Decode/index.php">Decode<span class="sr-only">(current)</span></a>
+      </li>
+	  <li class="nav-item active">
+        <a class="nav-link" href="./../Generate/index.php">Generate <span class="sr-only">(current)</span></a>
+      </li>
+     
+    </ul>
+   
+  </div>
+</nav>
+
+</header>
+	<div class="bg">
+        <br><br><br>
+        <div class="container row">
+            <div class="col-md-6 offset-md-3" style="background-color: white; padding: 20px; box-shadow: 10px 10px 5px #888;">
+                <div class="panel-heading">
+                    <h1>Decode QR codes</h1>
+                </div>
+                <hr>
+                <form action="decode.php" method="post" enctype="multipart/form-data">
+                    <input type="file" name="qrimage" accept="image/*" id="qrimage" class="form-control"><br>
+                    <input type="submit" class="btn btn-md btn-block btn-danger" value="Decode the Code">
+                </form>
+            </div>
+        </div>
+    </div>
+    
+</body>
+
+<script src="js/jquery-3.3.1.min.js" ></script>
+<script src="js/bootstrap.min.js" ></script>
+</html>
